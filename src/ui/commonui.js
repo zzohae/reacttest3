69e14724b30297ca91d031f627b59d70d5859071
zzohae <badahaeyeoncho@gmail.com>
2024-11-20 10:24:03 +0900

    버튼 스타일 수정

--- src/ui/commonui.js
+++ src/ui/commonui.js
@@ -5,34 +5,24 @@ export const Btn = styled.button`
   justify-content: center;
   align-items: center;
   gap: ${(props) => (props.version === 'v2' ? '4px' : '0')};
-  background-color: ${(props) => props.bgColor || '#214AEE'};
-  color: ${(props) => (props.bgColor === '#fff' ? '#214AEE' : '#fff')};
-  padding: 10px 30px;
-  height: 40px;
+  background-color: ${(props) => props.version==='v1' ? '#ffffff' : '#214AEE'};
+  color: ${(props) => (props.version === 'v1' ? '#214AEE' : '#fff')};
+  padding: 0 30px;
+  height: ${(props) => (props.version === 'v2' ? '45px' : '40px')};
   border: 3px solid;
-  border-color: ${(props) =>
-    props.version === 'v2'
-      ? '#FFEA7D'
-      : props.version === 'v3'
-      ? '#fff'
-      : '#214AEE'};
+  border-color: ${(props) => props.version === 'v2' ? '#FFEA7D' : props.version === 'v3' ? '#fff' : '#214AEE'};
   transition-property: color, border-color, background-color;
   word-break: keep-all;
   white-space: nowrap;
   border-radius: 30px;
   font-size: 1.125rem;
-  font-weight: 600;
+  font-weight: ${(props) => props.version === 'v2' ? '700' : '500'};
   cursor: pointer;
   transition: 0.5s;
 
   &:hover {
-    background-color: ${(props) =>
-      props.bgColor === '#fff'
-        ? '#214AEE'
-        : props.version === 'v2'
-        ? '#FFEA7D'
-        : '#fff'}; /* 조건부 색상 */
-    color: ${(props) => (props.bgColor === '#fff' ? '#fff' : '#214AEE')};
+    background-color: ${(props) => props.version === 'v2' ? '#FFEA7D' : props.version === 'v3' ? '#fff' : '#214AEE'};
+    color: ${(props) => (props.version === 'v1' ? '#fff' : '#214AEE')};
     font-weight: 700;
   }
 `
@@ -54,14 +44,14 @@ export const InCartBtn = styled.button`
 
 export const TagStyle = styled.div`
   margin-left: 0.5rem;
-  left: ${(props) => (props.index || 0) * 60}px; /* 태그 간 간격 조정 */
+  left: ${(props) => (props.index || 0) * 60}px;
   padding: 0.3rem 0.6rem;
   background-color: ${(props) =>
     props.type === 'hot'
       ? '#FF4500'
       : props.type === 'best'
       ? '#214AEE'
-      : '#FFEA7D'}; // NEW 배경색
+      : '#FFEA7D'};
   color: ${(props) => (props.type === 'new' ? '#214AEE' : 'white')};
   font-size: 0.75rem;
   font-weight: bold;
