138334f07194caec75b48914c9745e2d0e932694
zzohae <badahaeyeoncho@gmail.com>
2024-11-27 12:48:59 +0900

    메인 반응형 배너

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -4,11 +4,19 @@ import Boardlist from "./Boardlist";
 import allData from "../db/allData.json";
 
 export default function Home({ bgcolor, textcolor, vh }) {
+  const pagination1 = {
+    clickable: true,
+    renderBullet: function(index, className) {
+      return `<span class="${className}">${allData.mainRWDbanner.bannerData[index].alt}</span>`;
+    },
+  };
   return (
     <div style={{ background: bgcolor, color: textcolor, height: vh }}>
       <SwiperBanner
-      datakey={allData.bannerData}
+      datakey={allData.mainRWDbanner}
       viewslides={1}
+      pagination={pagination1}
+      hasrwd={true}
       ></SwiperBanner>
       <Boardlist datakey={allData.mainPagetitle}></Boardlist>
     </div>
--- src/contents/SwiperBanner.js
+++ src/contents/SwiperBanner.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
-export default function SwiperBanner({ datakey, viewslides }) {
+export default function SwiperBanner({ datakey, viewslides, pagination, hasrwd =false }) {
+
+  const { bannerData, bannerDataMd, bannerDataSm } = datakey;
+
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [responsiveData, setResponsiveData] = useState(bannerData);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    if (screenWidth < 576) {
+      setResponsiveData(bannerDataSm);
+    } else if (screenWidth < 992) {
+      setResponsiveData(bannerDataMd);
+    } else {
+      setResponsiveData(bannerData);
+    }
+
+    if (!hasrwd) {
+      setResponsiveData(bannerData);
+    }
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, [screenWidth, bannerData, bannerDataMd, bannerDataSm, hasrwd]);
+
   return (
     <Swiper
       modules={[Autoplay, Pagination, Navigation]}
@@ -14,22 +42,15 @@ export default function SwiperBanner({ datakey, viewslides }) {
         delay: 3000,
         disableOnInteraction: false,
       }}
-      pagination={{
-        clickable: true,
-        renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + datakey[index].alt + '</span>';
-        },
-      }}
+      pagination={pagination}
     >
-      {
-        datakey.map((v, i) => {
-          return (
-            <SwiperSlide key={i} style={{ width: "fit-content" }}>
-              <img src={v.src} alt={v.alt} />
-            </SwiperSlide>
-          );
-        })
-      }
+      { responsiveData.map((v, i) => (
+        <SwiperSlide key={i} style={{ width: 'fit-content' }}>
+          <img src={`/assets/img/banner/${v.src}`} alt={v.alt} />
+        </SwiperSlide>
+      ))   
+    
+    }
     </Swiper>
   );
 }
