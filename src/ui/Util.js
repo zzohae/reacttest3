a76cbfb7c0016249e45c9c2ae41c3bd02e38a8d0
LHE123 <dlgpdms6424@gmail.com>
2024-11-28 16:53:19 +0900

    svg cart변경

--- src/ui/Util.js
+++ src/ui/Util.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as AlertIcon } from '../svg/util/alert.svg';
 import { ReactComponent as CartIcon } from '../svg/util/cart.svg';
