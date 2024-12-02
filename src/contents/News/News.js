5b21acdcc1903db9dc22663f14d98986b146dbd0
KSH7131 <gimsh7131@gmail.com>
2024-12-02 15:38:06 +0900

    스타일병합해결

--- src/contents/News/News.js
+++ src/contents/News/News.js
@@ -1,45 +1,47 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
-import Event from './Event';
+import OnlineEvent from './Event';
 import Notice from './Notice';
 import LocalEvent from './LocalEvent';
-import CardNews from './CardNews';
+import CardNews from './CardNews'
 import allcategory from '../../db/allData.json';
 
 export default function News() {
-  const { en } = useParams();  // URL 파라미터에서 'en' 값을 추출
+  const { en } = useParams();
 
   const submenu = allcategory.navdata.promotionmenu.find(
     (menu) => menu.linkto === "news/notice"
   ).submenu;
 
-  // en 값에 맞는 컴포넌트를 반환하는 함수
   const getComponent = (en) => {
     switch (en) {
       case 'notice':
         return <Notice />;
       case 'online_event':
-        return <Event />;
+        return <OnlineEvent />;
       case 'offline_event':
         return <LocalEvent />;
       case 'cardnews':
         return <CardNews />;
       default:
+
         return <Notice />;
     }
   };
 
   return (
     <div className='d-flex flex-column align-items-center'>
-      <ul className="d-flex flex-wrap justify-content-center justify-content-xl-start mytab">
-        {submenu.map((v, i) => (
-          <li className={`d-flex justify-content-center align-items-center ${en === `${v.linkto}` ? 'active' : ''}`} key={i}>
-            <Link to={`/news/${v.linkto}`}>{v.title}</Link>
-          </li>
-        ))}
-      </ul>
-      <div className="content">
-        {getComponent(en)}  {/* en 값에 맞는 컴포넌트를 렌더링 */}
+      <div className='container mycontainer'>
+        <ul className="d-flex flex-wrap justify-content-center justify-content-xl-start mytab">
+          {submenu.map((v, i) => (
+            <li className={`d-flex justify-content-center align-items-center ${en === `${v.linkto}` ? 'active' : ''}`} key={i}>
+              <Link to={`/news/${v.linkto}`}>{v.title}</Link>
+            </li>
+          ))}
+        </ul>
+        <div>
+          {getComponent(en)}
+        </div>
       </div>
     </div>
   );
