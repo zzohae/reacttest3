590662948715ef84298c68bb3cd4c9f252deaf48
zzohae <badahaeyeoncho@gmail.com>
2024-12-03 17:25:59 +0900

    흐아앙

--- src/contents/Boardlist.js
+++ src/contents/Boardlist.js
@@ -5,7 +5,7 @@ import Best from '../layout/Best'
 
 export default function Boardlist() {
   return (
-    <div className='d-flex flex-column align-items-center'>
+    <div className='d-flex flex-column align-items-center overflow-hidden boardlist'>
       <MdsPick></MdsPick>
       <Best></Best>
     </div>
